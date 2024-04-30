8a3cbd8b244a686aa48b659f35de95a746662a52
Rafel Castello Fiol <rcastellofiol@gmail.com>
2024-04-30 14:18:53 +0200

    Versión 1.3.4

--- 005 Django/miProyecto/miProyecto/db.py
+++ 005 Django/miProyecto/miProyecto/db.py
@@ -1,11 +1,8 @@
 from django.db import models
 
 class Cliente(models.Model):
-    nombre = 
-models.CharField(max_length = 64)
-    apellidos = 
-models.CharField(max_length = 64)
+    nombre = models.CharField(max_length = 64)
+    apellidos = models.CharField(max_length = 64)
     rfc = models.CharField(max_length = 15, unique = True)
     fecha_nacimiento = models.DateField()
-    activo = 
-models.BooleanField(default = True)
+    activo = models.BooleanField(default = True)
